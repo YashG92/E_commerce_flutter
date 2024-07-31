b7f4a066ee93e073b218b273aebb2138847a3453
yashg92 <ygworld76@gmail.com>
2024-07-31 13:55:42 +0530

    tabbar in store screen for scrollable categories

---
+++ lib/common/widgets/brands/brand_card.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+import '../../../utils/constants/colors.dart';
+import '../../../utils/constants/image_strings.dart';
+import '../../../utils/constants/sizes.dart';
+import '../../../utils/helpers/helper_functions.dart';
+import '../custom_shapes/containers/rounded_container.dart';
+import '../images/t_circular_image.dart';
+import '../text/t_brand_title_text_with_verified_icon.dart';
+
+class TBrandCard extends StatelessWidget {
+  const TBrandCard({super.key, required this.showBorder, this.onTap});
+
+  final bool showBorder;
+  final void Function()? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
+    return GestureDetector(
+      onTap: onTap,
+      child: TRoundedContainer(
+        padding: EdgeInsets.all(TSizes.sm),
+        showBorder: showBorder,
+        backgroundColor: Colors.transparent,
+        child: Row(
+          children: [
+            ///Icon
+            Flexible(
+              child: TCircularImage(
+                isNetworkImage: false,
+                image: TImages.clothIcon,
+                backgroundColor: Colors.transparent,
+                overlayColor: THelperFunctions.isDarkMode(context)
+                    ? TColors.white
+                    : TColors.black,
+              ),
+            ),
+            SizedBox(
+              width: TSizes.spaceBtwItems / 2,
+            ),
+
+            ///Text
+            Expanded(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  TBrandTitleWithVerifiedIcon(
+                    title: 'Nike',
+                  ),
+                  Text(
+                    '256 products',
+                    overflow: TextOverflow.ellipsis,
+                    style: Theme.of(context).textTheme.labelMedium,
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/features/shop/screens/store/store.dart
+++ lib/features/shop/screens/store/store.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:t_store/common/widgets/appbar/appbar.dart';
+import 'package:t_store/common/widgets/brands/brand_card.dart';
 import 'package:t_store/common/widgets/custom_shapes/containers/rounded_container.dart';
 import 'package:t_store/common/widgets/custom_shapes/containers/search_container.dart';
 import 'package:t_store/common/widgets/layouts/grid_layout.dart';
 import 'package:t_store/common/widgets/products/cart/cart_menu_icon.dart';
 import 'package:t_store/common/widgets/text/section_heading.dart';
+import 'package:t_store/features/shop/screens/store/widgets/category_tab.dart';
 import 'package:t_store/utils/constants/colors.dart';
 import 'package:t_store/utils/constants/image_strings.dart';
 import 'package:t_store/utils/constants/sizes.dart';
 import 'package:t_store/utils/helpers/helper_functions.dart';
 
+import '../../../../common/widgets/appbar/tabbar.dart';
+import '../../../../common/widgets/brands/brand_show_case.dart';
 import '../../../../common/widgets/images/t_circular_image.dart';
 import '../../../../common/widgets/text/t_brand_title_text_with_verified_icon.dart';
 
@@ -18,17 +22,19 @@ class StoreScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: TAppBar(
-        title: Text(
-          'Store',
-          style: Theme.of(context).textTheme.headlineMedium,
+    return DefaultTabController(
+      length: 5,
+      child: Scaffold(
+        appBar: TAppBar(
+          title: Text(
+            'Store',
+            style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          actions: [
+            TCartCounterIcon(onPressed: () {}),
+          ],
         ),
-        actions: [
-          TCartCounterIcon(onPressed: () {}),
-        ],
-      ),
-      body: NestedScrollView(
+        body: NestedScrollView(
           headerSliverBuilder: (_, innerBoxIsScrolled) {
             return [
               SliverAppBar(
@@ -69,61 +75,52 @@ class StoreScreen extends StatelessWidget {
                         height: TSizes.spaceBtwItems / 1.5,
                       ),
 
-                      TGridLayout(itemCount: 4,mainAxisExtent: 80,  itemBuilder: (_,index) {
-                        return GestureDetector(
-                          onTap: (){},
-                          child: TRoundedContainer(
-                            padding: EdgeInsets.all(TSizes.sm),
-                            showBorder: true,
-                            backgroundColor: Colors.transparent,
-                            child: Row(
-                              children: [
-                                ///Icon
-                                Flexible(
-                                  child: TCircularImage(
-                                    isNetworkImage: false,
-                                    image: TImages.clothIcon,
-                                    backgroundColor: Colors.transparent,
-                                    overlayColor: THelperFunctions.isDarkMode(context)
-                                        ? TColors.white
-                                        : TColors.black,
-                                  ),
-                                ),
-                                SizedBox(
-                                  width: TSizes.spaceBtwItems / 2,
-                                ),
-
-                                ///Text
-                                Expanded(
-                                  child: Column(
-                                    mainAxisSize: MainAxisSize.min,
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: [
-                                      TBrandTitleWithVerifiedIcon(
-                                        title: 'Nike',
-                                      ),
-                                      Text(
-                                        '256 products',
-                                        overflow: TextOverflow.ellipsis,
-                                        style:
-                                        Theme.of(context).textTheme.labelMedium,
-                                      )
-                                    ],
-                                  ),
-                                )
-                              ],
-                            ),
-                          ),
-                        );
-                      })
-
+                      TGridLayout(
+                          itemCount: 4,
+                          mainAxisExtent: 80,
+                          itemBuilder: (_, index) {
+                            return TBrandCard(showBorder: true);
+                          }),
                     ],
                   ),
+                ),
+
+                ///Tabs
+                bottom: TTabBar(
+                  tabs: [
+                    Tab(
+                      child: Text('Sports'),
+                    ),
+                    Tab(
+                      child: Text('Furniture'),
+                    ),
+                    Tab(
+                      child: Text('Electronics'),
+                    ),
+                    Tab(
+                      child: Text('Clothes'),
+                    ),
+                    Tab(
+                      child: Text('Cosmetics'),
+                    ),
+                  ],
                 ),
               ),
             ];
           },
-          body: Container()),
+
+          ///Body
+          body: TabBarView(
+            children: [
+              TCategoryTab(),
+              TCategoryTab(),
+              TCategoryTab(),
+              TCategoryTab(),
+              TCategoryTab(),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
