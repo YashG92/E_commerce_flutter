fefbd50d83b7fc11fa1a0e6175e7fc502b707152
yashg92 <ygworld76@gmail.com>
2024-08-04 12:27:01 +0530

    checkout screen with billing section and success screen

--- lib/features/shop/screens/checkout/checkout.dart
+++ lib/features/shop/screens/checkout/checkout.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_navigation/get_navigation.dart';
 import 'package:t_store/common/widgets/appbar/appbar.dart';
 import 'package:t_store/common/widgets/custom_shapes/containers/rounded_container.dart';
+import 'package:t_store/common/widgets/sucess_screen/sucess_screen.dart';
 import 'package:t_store/features/shop/screens/cart/widgets/cart_items.dart';
+import 'package:t_store/features/shop/screens/checkout/widgets/billing_address_section.dart';
+import 'package:t_store/features/shop/screens/checkout/widgets/billing_amount_section.dart';
+import 'package:t_store/features/shop/screens/checkout/widgets/billing_payment_section.dart';
+import 'package:t_store/navigation_menu.dart';
 import 'package:t_store/utils/constants/colors.dart';
+import 'package:t_store/utils/constants/image_strings.dart';
 import 'package:t_store/utils/constants/sizes.dart';
 import 'package:t_store/utils/helpers/helper_functions.dart';
+
+import '../../../../common/widgets/products/cart/coupon_widget.dart';
 
 class CheckoutScreen extends StatelessWidget {
   const CheckoutScreen({super.key});
@@ -16,7 +26,7 @@ class CheckoutScreen extends StatelessWidget {
       appBar: TAppBar(
         showBackArrow: true,
         title: Text(
-          'Cart',
+          'Order Review',
           style: Theme.of(context).textTheme.headlineSmall,
         ),
       ),
@@ -34,52 +44,45 @@ class CheckoutScreen extends StatelessWidget {
               ),
 
               ///Coupons
-              TRoundedContainer(
-                showBorder: true,
-                backgroundColor: dark ? TColors.dark : TColors.white,
-                padding: EdgeInsets.only(
-                    top: TSizes.sm,
-                    bottom: TSizes.sm,
-                    right: TSizes.sm,
-                    left: TSizes.sm),
-                child: Row(
-                  children: [
-                    /// TextField
-                    Flexible(
-                      child: TextFormField(
-                        decoration: InputDecoration(
-                          hintText: 'Have a prome code? Enter here',
-                          focusedBorder: InputBorder.none,
-                          enabledBorder: InputBorder.none,
-                          errorBorder: InputBorder.none,
-                          disabledBorder: InputBorder.none,
-                        ),
-                      ),
-                    ),
-
-                    /// Button
-                    SizedBox(
-                      width: 80,
-                      child: ElevatedButton(
-                        onPressed: () {},
-                        style: ElevatedButton.styleFrom(
-                          foregroundColor: dark ? TColors.white.withOpacity(0.5) :  TColors.dark.withOpacity(0.5),
-                          backgroundColor: TColors.grey.withOpacity(0.2),
-                          side: BorderSide(color: TColors.grey.withOpacity(0.1)),
-                        ),
-                        child: Text('Apply'),
-                      ),
-                    )
-                  ],
-                ),
-              ),
+              TCouponCode(),
               SizedBox(
                 height: TSizes.spaceBtwSections,
+              ),
+
+              ///Billing
+              TRoundedContainer(
+                showBorder: true,
+                padding: EdgeInsets.all(TSizes.md),
+                backgroundColor: dark ? TColors.black : TColors.white,
+                child: Column(
+                  children: [
+                    ///Pricing
+                    TBillingAmountSection(),
+                    SizedBox(height: TSizes.spaceBtwItems,),
+                    ///Divider
+                    Divider(),
+                    SizedBox(height: TSizes.spaceBtwItems,),
+                    ///Payment Methods
+                    TBillingPaymentSection(),
+                    SizedBox(height: TSizes.spaceBtwItems,),
+
+                    ///Address
+                    TBillingAddressSection(),
+
+                  ],
+                ),
               )
             ],
           ),
         ),
       ),
+      ///Checkout Button
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(TSizes.defaultSpace),
+        child: ElevatedButton(onPressed: ()=> Get.to(()=> SuccessScreen(image: TImages.successfulPaymentIcon, title: 'Payment!', subtitle: 'Your item will be shipped soon', onPressed: ()=> Get.offAll(()=> NavigationMenu()))), child: Text('Checkout ₹25000')),
+      ),
     );
   }
 }
+
+
